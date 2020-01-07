e0745913150a567c2df9f2d64122201b691a46d0
Arash Thr <arashThr@outlook.com>
2020-01-07 22:47:07 +0330

    Update logger: Add files

--- src/logger.js
+++ src/logger.js
@@ -1,13 +1,36 @@
-const winston = require('winston')
+const { createLogger, format, transports } = require('winston')
+const { combine, timestamp, prettyPrint } = format
 const configs = require('./configs')
 
-const logger = winston.createLogger({
+const logger = createLogger({
+    format: combine(timestamp(), prettyPrint()),
     transports: [
-        new winston.transports.Console({
-            format: winston.format.simple()
-      })
+        new transports.File({
+            filename: 'data/info.log',
+            level: 'info'
+        }),
+        new transports.File({
+            filename: 'data/errors.log',
+            level: 'error'
+        }),
+        new transports.File({
+            filename: 'data/all.log',
+            level: 'debug'
+        })
     ]
 })
+
+if (configs.isInDev) {
+    logger.add(
+        new transports.Console({
+            level: 'debug',
+            format: format.combine(
+                format.colorize(),
+                format.simple()
+              )
+        })
+    )
+}
 
 logger.level = configs.logLevel
 
